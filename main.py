f62c561ea718069423ef96382f65fca09bbab8ad
empttyxd <karpycevmakar@gmail.com>
2025-01-18 20:22:57 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from horse import horse2
+from template import template
+from functable import func_table
+
+
+def main():
+    horse2("a1")
+    template(3, 3, 3)
+    func_table(1, 2,3)
+
+
+if __name__ == "__main__":
+    main()
