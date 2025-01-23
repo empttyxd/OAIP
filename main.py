629581f4b3f1e74f847ec7fdda342a734ca21284
empttyxd <karpycevmakar@gmail.com>
2025-01-23 21:39:21 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from horse import horse2
-from template import template
-
-
+from groundhog import groundhog_day
+from gears import gears
+from asd import brackets
 def main():
-    horse2("a1")
-    template(3, 3, 3)
+
+        data1 = ['Groundhog Festival in Punxsutawney.',
+                'Groundhog Festival in Punksutawney.',
+                'Groundhog Festivel in Punxsutowney.']
+
+        data2 = [[0, 2, 30, 15], [14, 3, 21, 60], [7, 16, 4, 8]]
+
+        line = "[12 / (9) + 2(5{15 * <2 - 3>}6)]"
+
+        print(groundhog_day(data1))
+        print(gears(data2, 30, 7))
+        print(brackets(line))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
