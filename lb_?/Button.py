573f223a5fddecc2ec815d52aeae40119af8ae36
empttyxd <karpycevmakar@gmail.com>
2025-03-06 10:49:58 +0900

    Button.py

--- lb_?/Button.py
+++ lb_?/Button.py
@@ -1,10 +1,31 @@
 class button:
     def __init__(self):
         self.status = 'Unclicked'
+        self.color = 'Red'
+        self.form = 'Rectangle'
+        self.width = 5
+        self.length = 10
+        self.size = f'{self.length} x {self.width}'
+        self.x = -2
+        self.y = 1
+        self.position = f'{self.x}:{self.y}'
+
+    def change_position(self,x, y):
+        self.x = x
+        self.y = y
+
+    def change_size(self, width, length):
+        self.width = width
+        self.length = length
+
+    def change_color(self, color):
+        self.color = color
+
 
     def click(self):
         if self.status == 'Unclicked':
             self.status = 'Clicked'
+
     def function(self):
         if self.status == 'Clicked':
             print('did you think something would happen here? its just a button')
