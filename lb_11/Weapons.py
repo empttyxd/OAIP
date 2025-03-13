dc8ef50cb5ab2882f61eb4c24cd77c56fc0264ee
empttyxd <karpycevmakar@gmail.com>
2025-03-13 09:33:43 +0900

    Add files via upload

---
+++ lb_11/Weapons.py
@@ -0,0 +1,97 @@
+class Weapons:
+    pass
+
+
+
+class Cold_weapons(Weapons):
+    pass
+
+class Ranged_weapons(Weapons):
+    pass
+
+
+
+class Swords(Cold_weapons):
+    pass
+
+class Long_swords(Swords):
+    pass
+
+class Short_swords(Swords):
+    pass
+
+class Hunting_swords(Swords):
+    pass
+
+class Sabers(Swords):
+    pass
+
+
+
+
+class Heavy_weapons(Cold_weapons):
+    pass
+
+class Axes(Heavy_weapons):
+    pass
+
+class Maces(Heavy_weapons):
+    pass
+
+class Hammers(Heavy_weapons):
+    pass
+
+
+
+
+class Polearms(Cold_weapons):
+    pass
+
+class Halberds(Polearms):
+    pass
+
+class Glaives(Polearms):
+    pass
+
+class Spears(Polearms):
+    pass
+
+
+
+class Bows(Ranged_weapons):
+    pass
+
+class Hunting_bows(Bows):
+    pass
+
+class Long_bows(Bows):
+    pass
+
+class Short_bows(Bows):
+    pass
+
+class Light_bows(Bows):
+    pass
+
+
+
+class Crossbows(Ranged_weapons):
+    pass
+
+class Heavy_crossbows(Crossbows):
+    pass
+
+class Light_crossbows(Crossbows):
+    pass
+
+
+class Guns(Ranged_weapons):
+    pass
+
+class Hook_gun(Guns):
+    pass
+
+
+
+
+
