9f203caa932da9c3c9aef7bc5d59750d7e84b217
empttyxd <karpycevmakar@gmail.com>
2025-03-14 12:56:50 +0900

    Add files via upload

---
+++ lb_11/web.py
@@ -0,0 +1,19 @@
+class Widget:
+    pass
+
+class Label(Widget):
+    pass
+
+class LineEdit(Label):
+    pass
+
+class TextEdit(Label):
+    pass
+
+class Button(Widget):
+    pass
+
+class CheckBox(Button):
+    pass
+
+class RadioButton(Button):
