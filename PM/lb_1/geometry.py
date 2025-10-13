f8d7d114a1088d1eed39fa185e10a6be8c65c863
empttyxd <karpycevmakar@gmail.com>
2025-10-14 08:12:59 +0900

    geometry.py

--- PM/lb_1/geometry.py
+++ PM/lb_1/geometry.py
@@ -1,26 +1,46 @@
-class Geom_obj:
+class Object:
     pass
 
-class Dot(Geom_obj):
+
+class Point(Object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line(Object):
+    def __init__(self, x1, x2, y1, y2):
+        self.first = Point(x1, y1)
+        self.second = Point(x2, y2)
+
+
+class Flat_Shape(Object):
     pass
 
-class Straight(Geom_obj):
-    pass
 
-class Flat_figure(Geom_obj):
-    pass
+class Ray(Line, Point):
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(x1, y1, x2, y2)
 
-class Ray(Dot):
-    pass
 
-class segment(Dot, Straight):
-    pass
+class Segment(Line, Point):
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(x1, y1, x2, y2)
 
-class Polygon(Dot, Flat_figure):
-    pass
+
+class Polygon(Point, Flat_Shape):
+    def __init__(self, *args):
+        Point.__init__(self, *args[0])
+        self.coordinates = args
+
 
 class Rectangle(Polygon):
-    pass
+    def __init__(self, x, y, width, height):
+        super().__init__((x, y), (x + width, y + height))
+        self.side_a = width
+        self.side_b = height
+
 
 class Square(Rectangle):
-    pass
+    def __init__(self, x, y, side_a):
+        super().__init__(x, y, x + side_a, y + side_a)
