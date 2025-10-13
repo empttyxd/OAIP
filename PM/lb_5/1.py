5e29ddc82a0a106735aac1594f2050857911c923
empttyxd <karpycevmakar@gmail.com>
2025-10-14 08:53:21 +0900

    Create 1.py

---
+++ PM/lb_5/1.py
@@ -0,0 +1,21 @@
+from typing import Generic, TypeVar
+
+
+A = TypeVar('T')
+
+
+class Point(Generic[A]):
+    def __init__(self, name: str, x: A, y: A, z: A):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        return f'{self.name}({self.x}, {self.y}, {self.z})'
+
+
+a1 = Point[int]('A', 1, 2, 3)
+a2 = Point[float]('B', 1.1, 2.2, 3.3)
+print(a1)
+print(a2)
