3987718a61e331c4eaec4044c4f906957f455076
empttyxd <karpycevmakar@gmail.com>
2025-10-14 08:28:54 +0900

    1.py

--- PM/lb_3/1.py
+++ PM/lb_3/1.py
@@ -1,1 +1,9 @@
+def factorial(n):
+    if n < 0:
+        return 'error'
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
+print('factorial', 5, 'is', factorial(5))
